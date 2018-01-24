257632c4bdbb48bb04af15aa4b47a90ce362de6b
aalekh <aalekh.nigam@gmail.com>
2018-01-24 22:15:37 +0530

    Implemented World's Simplest SQL compiler and Virtual Machine

--- src/speck.cpp
+++ src/speck.cpp
@@ -2,11 +2,63 @@
 #include <string>
 #include <iostream>
 
+enum MetaCommandResult_t { META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND };
+typedef enum MetaCommandResult_t MetaCommandResult;
+
+enum PrepareResult_t { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT };
+typedef enum PrepareResult_t PrepareResult;
+
+enum StatementType_t { STATEMENT_SELECT, STATEMENT_INSERT };
+typedef enum StatementType_t StatementType;
+
+struct Statement_t {
+	StatementType type;
+};
+typedef struct Statement_t Statement;
+
 struct InputSpeck {
 	std::string buffer;
 	uint64_t buffer_length;
 	// uint64_t input_length;
 };
+
+MetaCommandResult do_meta_command(InputSpeck* input_speck) {
+	if(input_speck->buffer == ".exit") {
+		std::cout << "Goodbye btw I was Skynet all along !!\n";
+		exit(EXIT_SUCCESS);
+	} else {
+		return META_COMMAND_UNRECOGNIZED_COMMAND;
+	}
+}
+
+PrepareResult prepare_statement(InputSpeck* input_speck, Statement* statement) {
+	// Remember to add Error Handling here
+	std::string stat_type = input_speck->buffer.substr(0, 6);
+	if(stat_type == "insert") {
+		statement->type = STATEMENT_INSERT;
+		return PREPARE_SUCCESS;
+	} else if (stat_type == "select") {
+		statement->type = STATEMENT_SELECT;
+		return PREPARE_SUCCESS;
+	}
+
+	return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+// This is going to be Virtual Machine
+void execute_statement(Statement* statement) {
+	switch(statement->type) {
+		case(STATEMENT_INSERT):
+			std::cout << "This is going to be an insert Statement" << std::endl;
+			break;
+		case(STATEMENT_SELECT):
+			std::cout << "This is going to be select statement" << std::endl;
+			break;
+	}
+	// No need for fallback mechanism here (atleast that's what I think for now)
+}
+
+
 
 void initiateSpeck() {
 	const time_t ctt = time(0);
@@ -22,7 +74,6 @@ void prompt() {
 std::string read_text()
 {
     std::string prompt;
-    // cout << "Enter your text: ";
     std::getline(std::cin, prompt);
     return prompt;
 }
@@ -42,12 +93,32 @@ int main() {
 	while(true) {
 		prompt();
 		read_input(input_speck);
-		if(input_speck->buffer == ".exit"){
-			std::cout << "\n Goodbye Skynet !!" << std::endl;
-			break;
-		} else {
-			std::cout << "Something will happen just wait" << std::endl;
+		// if(input_speck->buffer == ".exit"){
+		// 	std::cout << "\n Goodbye btw I was Skynet all along !!" << std::endl;
+		// 	exit(EXIT_SUCCESS);
+		// } else {
+		// 	std::cout << "Something will happen just wait" << std::endl;
+		// }
+		if(input_speck->buffer.substr(0,1) == ".") {
+			switch(do_meta_command(input_speck)) {
+				case (META_COMMAND_SUCCESS):
+					continue;
+				case (META_COMMAND_UNRECOGNIZED_COMMAND):
+					std::cout << "Unrecognised command " << input_speck->buffer << std::endl;
+			}
 		}
+
+		Statement statement;
+		switch(prepare_statement(input_speck, &statement)) {
+			case(PREPARE_SUCCESS):
+				break;
+			case(PREPARE_UNRECOGNIZED_STATEMENT):
+				std::cout << "Buddy you typed something wrong :/, take a look back here: " << input_speck->buffer << std::endl;
+				continue;
+		}
+		execute_statement(&statement);
+		std::cout << "Executed" << std::endl;
+
 	}
 	return 0;
 }
